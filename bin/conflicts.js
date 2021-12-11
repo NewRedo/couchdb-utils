c1c3fb0486ed5e6e4434d50967463f4810104735
Phill Luby (NewRedo Ltd) <phill.luby@newredo.com>
2021-12-11 19:01:58 +0000

    Report information to STDERR when extracting conflicts.

--- bin/conflicts.js
+++ bin/conflicts.js
@@ -16,6 +16,8 @@ const { strict } = require('assert');
 const path = process.argv[2];
 const db = new PouchDB(path);
 
+const LIMIT = 100;
+
 (async function() {
     const conflicts = await db.find({
         "selector": {
@@ -23,17 +25,23 @@ const db = new PouchDB(path);
         }, 
         "conflicts": true, 
         "fields": [ "_id", "_rev", "_conflicts" ], 
-        "limit": 100 
+        "limit": LIMIT
     });
+    console.warn(`${conflicts.docs.length} documents found.`);
+    if (conflicts.docs.length === LIMIT) {
+        console.warn(`Limit of ${LIMIT} reached, there may be more.`);
+    }
     for(let doc_id of conflicts.docs) {
         const doc = await db.get(doc_id._id, {rev:doc_id._rev});
         const filename = `${doc._id} rev-${doc._rev} primary.json`;
         fs.writeFileSync(filename, JSON.stringify(doc, null, 4));
+        console.warn(`Wrote ${filename}.`);
         
         for (let conflict of doc_id._conflicts) {
             const doc = await db.get(doc_id._id, {rev:conflict});
             const filename = `${doc._id} rev-${doc._rev} conflict.json`;
             fs.writeFileSync(filename, JSON.stringify(doc, null, 4));
+            console.warn(`Wrote ${filename}.`);
         }
     }
 })().catch(console.warn);
